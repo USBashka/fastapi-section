a0fd2c9f26f66193b54ba6b9571c8e7cff450840
USBashka <usbashka@gmail.com>
2025-12-15 21:48:31 +0400

    Написаны заготовки эндпоинтов

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/get_last_trading_days")
+async def get_last_trading_days(count: int) -> list[str]:
+    result: list[str] = []
+
+    return result
+
+
+@app.get("/get_dynamics")
+async def get_dynamics(oil_id: int,
+                       delivery_type_id: int,
+                       delivery_basis_id: int,
+                       start_date: str,
+                       end_date: str) -> list:
+    result: list = []
+
+    return result
+
+
+@app.get("/get_trading_results")
+async def get_trading_results(oil_id: int,
+                              delivery_type_id: int,
+                              delivery_basis_id: int) -> list:
+    result = []
+
+    return result
